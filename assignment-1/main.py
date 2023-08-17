3faf73be9e3bb301a0d26500eaff1d5ec0825e25
Sourav  Padhi <97221197+heysouravv@users.noreply.github.com>
2023-08-17 19:28:42 +0530

    Go big or go home

--- assignment-1/main.py
+++ assignment-1/main.py
@@ -5,25 +5,25 @@ import os
 
 # Base64 encoded C code for printing "Hello, World!"
 encoded_c_code = b'''
-I2luY2x1ZGUgPHN0ZGlvLmg+CmludCBtYWluKCkgewogICAgcHJpbnRmKCJIZWxsbywgV29ybGQhXG4iKTsKICAgIHJldHVybiAwOwp9Cg==
+I2luY2x1ZGUgPHN0ZGlvLmg+CmludCBtYWluKCkgewogICAgcHJpbnRmKCJIZWxsbywgVW5pdmVyc2UhXG4iKTsKICAgIHJldHVybiAwOwp9
 '''
 
 # Decode the base64 encoded C code
 decoded_c_code = base64.b64decode(encoded_c_code).decode('utf-8')
 
 # Save the decoded C code to a temporary file
-with open('temp_hello.c', 'w') as f:
+with open('temp.c', 'w') as f:
     f.write(decoded_c_code)
 
 # Compile the C code
-subprocess.run(['gcc', 'temp_hello.c', '-o', 'temp_hello'])
+subprocess.run(['gcc', 'temp.c', '-o', 'temp_file'])
 
 # Load the compiled C code as a shared library
-hello_lib = ctypes.CDLL('./temp_hello')
+hello_lib = ctypes.CDLL('./temp_file')
 
 # Call the main function to print "Hello, World!"
 hello_lib.main()
 
 # Cleanup: remove the temporary files
-os.remove('temp_hello.c')
-os.remove('temp_hello')
+os.remove('temp.c')
+os.remove('temp_file')
